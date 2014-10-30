83acb61ed2d13bc3f3c3198681441dec432fc3fc
Emilio Cobos Álvarez <ecoal95@gmail.com>
2014-10-30 10:54:30 +0100

    Update application.js

--- js/application.js
+++ js/application.js
@@ -24,30 +24,41 @@ app.registerStep('Parse Blogger data', function(step, posts) {
 	step.resolve();
 });
 
-(function() {
-	var container = document.getElementById('main-progress'),
+app.on('complete', function() {
+	app.showMessage('Migration completed!');
+	elements.bar.value = 100;
+});
+
+app.on('step.change', (function() {
+	var container = document.getElementById('step'),
 		elements;
 	elements = {
-		current: container.querySelector('.progress__current'),
-		stepCount: container.querySelector('.progress__count'),
-		description: container.querySelector('.progress__description'),
-		bar: container.querySelector('.progress__bar')
+		current: container.querySelector('.step__current'),
+		stepCount: container.querySelector('.step__count'),
+		description: container.querySelector('.step__description'),
+		bar: container.querySelector('.step__bar')
 	};
-
-	app.on('complete', function() {
-		app.showMessage('Migration completed!');
-		elements.bar.value = 100;
-	});
-
-	app.on('step.change', function(step) {
+	return function(step) {
 		var current = app.steps.indexOf(step) + 1;
 
 		elements.current.innerHTML = current;
 		elements.stepCount.innerHTML = app.steps.length;
 		elements.description.innerHTML = step.description;
 		elements.bar.value = (current - 1) / app.steps.length * 100;
-	});
-}());
+	}
+}()));
+
+app.on('message', (function() {
+	var log = document.getElementById('log');
+
+	return function(message, type) {
+		var msg = document.createElement('div');
+
+		msg.className = 'message message--' + type;
+		msg.textContent = message;
+		log.appendChild(msg);
+	}
+} ()));
 
 
 document.getElementById('start-button').addEventListener('click', function(e) {
@@ -59,7 +70,7 @@ document.getElementById('start-button').addEventListener('click', function(e) {
 	}
 
 	document.getElementById('start').style.display = 'none';
-	document.getElementById('main-progress').style.display = '';
+	document.getElementById('import').style.display = '';
 
 	reader = new FileReader();
 
@@ -75,4 +86,3 @@ document.getElementById('start-button').addEventListener('click', function(e) {
 	reader.readAsText(file);
 
 }, false);
-
